110608c7471018fb240671b7d1854b6028076aa2
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2017-09-22 14:34:55 -0400

    Pushing "Hello World" for the first time :)

---
+++ Unit 1/src/jonah/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package jonah;
+
+public class HelloWorld {
+	
+	/** September 21, 2017
+	 * Hello World
+	 * Says Hello World
+	 * Aidan
+	 */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+
+	}
+
+}
