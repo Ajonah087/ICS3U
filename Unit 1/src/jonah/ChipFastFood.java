d83c82c2fa24992ed6800c7b61df6879d9983a62
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2017-09-28 15:01:26 -0400

    these are sorta correct programs

---
+++ Unit 1/src/jonah/ChipFastFood.java
@@ -0,0 +1,26 @@
+package jonah;
+import java.util.Scanner;
+
+public class ChipFastFood {
+
+Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Scanner scan= new Scanner (System.in);
+		System.out.println("What burger do you want? Choose a number from 1 to 4");
+		int menuBurger = scan.nextInt();
+		System.out.println("What drink do you want? Choose a number from 1 to 4");
+		int menuDrink = scan.nextInt();
+		System.out.println("What side order do you want? Choose a number from 1 to 4");
+		int menuSideOrder = scan.nextInt();	
+		System.out.println("What dessert do you want? Choose a number from 1 to 4");
+		
+		int Cheeseburger= 461;
+		int FishBurger= 431;
+		int VeggieBurger= 420;
+		int NoBurger = 0;
+		
+		if (menuBurger==1);
+}
+	}
---
+++ Unit 1/src/jonah/Goldilocks.java
@@ -0,0 +1,30 @@
+package jonah;
+import java.util.Scanner;
+
+public class Goldilocks {
+	
+Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Scanner scan= new Scanner (System.in);
+		System.out.println("What is the weight of the first bowl?");
+		int firstBowl = scan.nextInt();
+		System.out.println("What is the weight of the second bowl?");
+		int twoBowl = scan.nextInt();
+		System.out.println("What is the weight of the third bowl?");
+		int thirdBowl = scan.nextInt();
+		
+		if (firstBowl > twoBowl && twoBowl < thirdBowl) {
+			System.out.println("Mama bear's bowl is" + twoBowl);
+			
+		else if (twoBowl > firstBowl && firstBowl < thirdBowl)
+			System.out.println("Mama bear's bowl is" + firstBowl);
+		
+		else if (thirdBowl > twoBowl && thirdBowl < firstBowl)
+			System.out.println("Mama bear's bowl is" + thirdBowl);
+		
+		else
+			System.out.println("Please input 3 different numbers");
+}}}
+	
