a05a3c7a3684014061e5b40d14a8ee71eff7d8ac
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2017-10-03 12:17:33 -0400

    thank the lord... ive finished the 1st dots thingy

--- Unit 1/src/jonah/Trident_dots.java
+++ Unit 1/src/jonah/Trident_dots.java
@@ -7,39 +7,59 @@ public class Trident_dots {
 	public static void main(String[] args) {
 
 		Scanner scan= new Scanner (System.in);
-		System.out.println("What is the height of the trident?");
+		System.out.println("What is the length of the handle?");
 		int h = scan.nextInt();
 		System.out.println("What is the spacing of the trident?");
 		int s = scan.nextInt();
-		System.out.println("What is the length?");
+		System.out.println("What is the height of the prongs?");
 		int t = scan.nextInt();
 
 
-
-
-		for (int i=0; i < h; i++ ) {
+		for (int r=0; r < t; r++) {
+				System.out.print("*");
+				for (int a=0; a < s; a++) 
+					System.out.print(" ");
+				System.out.print("*");
+				for (int a=0; a < s; a++) 
+					System.out.print(" ");
+				System.out.println("*");
+		}
+		
+		for (int x=0; x < 2 * s + 3; x++) {
 			System.out.print("*");
-			for (int a=0; a < s; a++) 
+		}
+		
+			System.out.println();
+		for (int x=0; x < h; x++) {
+			for (int e=0; e < s + 1; e++) {
 				System.out.print(" ");
+			}
+			System.out.println("*");
 		}
-		for (int i=0; i < h; i++ ) {
-			System.out.print("*");
-			for (int a=0; a < s; a++) 
-				System.out.print(" ");	
-			System.out.print("*");
-
-
-
-			for (int e=0 ; e<t; e++) 
-				System.out.println();
-
-
-
-
-
-
-
-
 		}
-	}
-}
+			
+				//System.out.print("*");
+				//for (int e=0; e < s; e++) {	
+				//System.out.print(" ");
+				//System.out.print("*");
+			}
+		
+
+
+
+
+		//for (int e=0 ; e<t; e++) 
+			//System.out.println();
+
+
+
+
+
+
+
+
+
+	
+
+
+
