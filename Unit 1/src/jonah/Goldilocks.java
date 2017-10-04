b40303c7e280de610ec9b823eed013ef07b3512e
Ajonah087 <31707458+Ajonah087@users.noreply.github.com>
2017-10-03 20:01:44 -0400

    Update Goldilocks.java

--- Unit 1/src/jonah/Goldilocks.java
+++ Unit 1/src/jonah/Goldilocks.java
@@ -2,31 +2,22 @@ package jonah;
 import java.util.Scanner;
 
 public class Goldilocks {
-		
-	public static void main(String[] args) {
-		
-		Scanner scan= new Scanner (System.in);
-		System.out.println("What is the weight of the first bowl?");
-		int firstBowl = scan.nextInt();
-		System.out.println("What is the weight of the second bowl?");
-		int twoBowl = scan.nextInt();
-		System.out.println("What is the weight of the third bowl?");
-		int thirdBowl = scan.nextInt();
-		
-		if (firstBowl > twoBowl && twoBowl < thirdBowl) {
-			System.out.println("Mama bear's bowl is" + twoBowl); 
-		}	
-		else if (twoBowl > firstBowl && firstBowl < thirdBowl)
-			System.out.println("Mama bear's bowl is" + firstBowl); 
-		
-		else if (twoBowl > thirdBowl && thirdBowl < firstBowl)
-			System.out.println("Mama bear's bowl is, " + thirdBowl); 
-		
-		else
-			System.out.println("Please input 3 different numbers");
-
-		
-		}
-	}
+  
+ public static void main(String[] args) {
+  
+  Scanner scan= new Scanner (System.in);
+  System.out.println("What is the weight of the first bowl?");
+  int bowl1 = scan.nextInt();
+  System.out.println("What is the weight of the second bowl?");
+  int bowl2 = scan.nextInt();
+  System.out.println("What is the weight of the third bowl?");
+  int bowl3 = scan.nextInt();
+  
+  if (bowl1 > bowl2 && bowl2 > bowl3 || bowl3 > bowl2 && bowl1 < bowl2) {
+   System.out.println("Mama bear's bowl is " + bowl2); 
+  } 
+  else if (bowl2 > bowl1 && bowl1 > bowl3 || bowl2 < bowl1 && bowl3 > bowl1) System.out.println("Mama bear's bowl is " + bowl1); 
+  
+  else if (bowl2 > bowl3 && bowl3 > bowl1 || bowl2 < bowl3 && bowl3 < bowl1)System.out.println("Mama bear's bowl is, " + bowl3); 
 
 	
