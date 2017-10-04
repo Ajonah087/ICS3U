695e2b112d6c501416cdfa447c3851d333d30900
Ajonah087 <31707458+Ajonah087@users.noreply.github.com>
2017-10-03 20:03:27 -0400

    Update ISBN.java

--- Unit 1/src/jonah/ISBN.java
+++ Unit 1/src/jonah/ISBN.java
@@ -4,8 +4,22 @@ import java.util.Scanner;
 
 public class ISBN {
 	
-	public static void main(String[] args) {
+ public static void main(String[] args) {
+	 
+int total = 9*1+7*3+8*1+0*3+9*1+2*3+1*1+4*3+1*1+8*3;
+int num1;
+int num2;
+int num3;
 
-	Scanner scan= new Scanner (System.in);
-
-}}
+ Scanner scan= new Scanner (System.in);
+ System.out.println("Please enter a value for number 1: ");
+ num1 = scan.nextInt();
+ System.out.println("Please enter a value for number 2: ");
+ num2 = scan.nextInt();
+ System.out.println("Please enter a value for number 3: ");
+ num3 = scan.nextInt();
+ num1 = num1 * 1;
+ num2 = num2 * 3;
+ num3 = num3 * 1;
+ total = total + num1 + num2 + num3;
+ 
