2deaf334c464f536894f4b96d6f0ef46527d2792
Ajonah087 <31707458+Ajonah087@users.noreply.github.com>
2017-10-03 20:05:37 -0400

    Update DadNumbers.java

--- Unit 1/src/jonah/DadNumbers.java
+++ Unit 1/src/jonah/DadNumbers.java
@@ -1,25 +1,32 @@
 package jonah;
 import java.util.Scanner;
-//Find number of answers
 
-public class DadNumbers {
-	
-	public static void main(String[] args) {
-		
-		int totalAnswer=0;
-		
-	Scanner scan= new Scanner (System.in);
-	System.out.println("Which number does Natalie ask her dad for?");
-	int natalieNumber= scan.nextInt();
-	
-	for (int i=1; natalieNumber >= 1 && natalieNumber <= 10;i++) {
-		
-		totalAnswer= natalieNumber % i;
-		
-		if (totalAnswer==0) {
-			System.out.println("" + totalAnswer);
-			
-		}
-}
-	}
+public class DadNumber {
+ 
+ public static void main(String[] args) {
+
+   int natalieNumber;
+   
+ Scanner scan= new Scanner (System.in);
+ 
+  System.out.println("What number does Natalie's dad give her? ");
+  natalieNumber = scan.nextInt();
+  
+  if (natalieNumber == 1) {
+   System.out.println("1"); 
+  } else if (natalieNumber == 2 || natalieNumber == 3) {
+   System.out.println("2"); 
+  } else if (natalieNumber == 4 || natalieNumber == 5) {
+   System.out.println("3"); 
+  } else if (natalieNumber == 6 || natalieNumber == 7) {
+   System.out.println("4"); 
+  } else if (natalieNumber == 8 || natalieNumber == 9) {
+   System.out.println("5"); 
+  } else if (natalieNumber == 10) {
+   System.out.println("6"); 
+  } else {
+   System.out.println("Invalid Input."); 
+  }
+  
+  }
 }
