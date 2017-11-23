d4e3cd9f7ad592a967b67acc552d9764cf8646f2
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2017-11-23 14:29:41 -0500

    here befor major changes

--- Unit 2/src/jonah/TicTacToe.java
+++ Unit 2/src/jonah/TicTacToe.java
@@ -24,62 +24,71 @@ public class TicTacToe {
 
 
 		for (int i=0; i < 5; i++) {
-			System.out.println("Starting with X, input the row and column you have chosen");
+			System.out.println("Starting with X, input the row and column you have chosen, pick number between 1-3");
 			int row = scan.nextInt();
 			int column= scan.nextInt();
 			specSpace=0;
-			
+
 			gameSpaces[row-1][column-1]= specSpace;
 			int arrayXY= (specSpace);
 			boolean checkX = gameSpacesX(row,column);
 			boolean checkY = gameSpacesY(row,column);
-		
-		if (gameSpacesX(row,column)==false) {
-			System.out.println("This spot has already been selected");
-		}
-		else if (gameSpacesX(row,column)==true) {
-			
-		}
-			
-		if (gameSpacesY(row,column)==false) {
-			System.out.println("This spot has already been selected");
-		}
-		else if (gameSpacesY(row,column)==true) {
-			
-		}
-		
-		for (int e=0; e< gameSpaces.length; e++) {
-			if (noSame(gameSpaces)==true) {
-				System.out.println("You have won the game!");
+
+			for (int e=0; e < 5; e++) {
+				if (gameSpacesX(row,column)==false) {
+					System.out.println("This spot has already been selected");
+				}
+				else if (gameSpacesX(row,column)==true) {
+
+				}
+
+				if (gameSpacesY(row,column)==false) {
+					System.out.println("This spot has already been selected");
+				}
+				else if (gameSpacesY(row,column)==true) {
+
+				}
+
+				for (int a=0; a< gameSpaces.length; a++) {
+					if (noSame(gameSpaces)==true) {
+						System.out.println("You have won the game!");
+					}
+					else {
+
+					}
+				}
+				for (int z=0; z < 3; z++) { 
+				System.out.println("    |     |     ");
+				}
+
 			}
 		}
-		}
-//		if (truth==false) {
-//			System.out.println("This spot has already been selected");
-//		}
-//		
-//		else if (ans==true) {
-//			System.out.println("You have won the game!");
-//		}
-//		else {
-//			
-//		}
-//		
-//		}
+		//		if (truth==false) {
+		//			System.out.println("This spot has already been selected");
+		//		}
+		//		
+		//		else if (ans==true) {
+		//			System.out.println("You have won the game!");
+		//		}
+		//		else {
+		//			
+		//		}
+		//		
+		//		}
 	}
-	
 
-	
+
+
 	public static boolean gameSpacesX (int row, int column) {
 		boolean truth= false;
 		int [][] gameSpaces = new int[3][3];
 		if (gameSpaces[row][column] == 0) {
 			truth=true;
 			gameSpaces[row-1][column-1] = 'X';
-		if (gameSpaces[row-1][column-1] != 0) {
-			System.out.println("This spot has already been selected");
-			truth=false;
-		}
+			if (gameSpaces[row-1][column-1] != 0) {
+				System.out.println("This spot has already been selected");
+				truth=false;
+			}
 		}
 		return truth;
 	}
@@ -90,17 +99,17 @@ public class TicTacToe {
 		if (gameSpaces[row][column] == 0) {
 			truth=true;
 
-		if (gameSpaces[row-1][column-1] != 0) {
-			gameSpaces[row-1][column-1] = 'Y';
-			System.out.println("This spot has already been selected");
-			truth=false;
+			if (gameSpaces[row-1][column-1] != 0) {
+				gameSpaces[row-1][column-1] = 'Y';
+				System.out.println("This spot has already been selected");
+				truth=false;
 
-		}
+			}
 		}
 		return truth;
 	}
 
-	public static boolean noSame (int[][] gameSpaces) {
+	public static boolean noSame (int [][] gameSpaces) {
 		boolean ans=false;
 		for (int i=0; i < gameSpaces.length; i++) {
 			if ((gameSpaces[i][0]==gameSpaces[i][1] && gameSpaces[i][1]==gameSpaces[i][2])) {
@@ -124,18 +133,18 @@ public class TicTacToe {
 					ans=true;
 					break;
 				}
-			else if ((gameSpaces[2][0]==gameSpaces[1][1] && gameSpaces[1][1]==gameSpaces[0][2])) {
+				else if ((gameSpaces[2][0]==gameSpaces[1][1] && gameSpaces[1][1]==gameSpaces[0][2])) {
 					ans=true;
 					break;
+				}
+				else {
+					ans=false;
+				}
+
 			}
-			else {
-				ans=false;
-			}
-				
 		}
-	}
 		return ans;
-}
+	}
 }
 
 
