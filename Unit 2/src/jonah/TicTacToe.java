4fd328622cb946d0bdf9f676b064bc52a54783e7
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2017-12-13 11:39:46 -0500

    ok

--- Unit 2/src/jonah/TicTacToe.java
+++ Unit 2/src/jonah/TicTacToe.java
@@ -15,7 +15,7 @@ public class TicTacToe {
 	 * @param args
 	 */
 
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean ans;
@@ -25,61 +25,75 @@ public class TicTacToe {
 		boolean checkX = true;
 		boolean checkY = true;
 
-
+		
+		System.out.print("Is X, or O, starting off the tic-tac-toe game?");
+		turn=scan.next().charAt(0);
+		System.out.println("Starting with " + turn + " input the row and column you have chosen, pick number between 1-3");
 		for (int i=0; i < 5; i++) {
-			System.out.println("Starting with X, input the row and column you have chosen, pick number between 1-3");
-			while (checkX || checkY == false)
-				System.out.println("Starting with X, input the row and column you have chosen, pick number between 1-3");
+			while (checkX || checkY == false ) {
 				int row = scan.nextInt();
 				int column= scan.nextInt();
-	
+
 				if (turn == 'X') {
 					checkX = gameSpacesX(row,column,gameSpaces);
 				}
 				else {
 					checkY = gameSpacesY(row,column);			
 				}
-				
+
 				if (checkX==false) {
 					System.out.println("This spot has already been selected");
 				}
 				if (checkY==false) {
 					System.out.println("This spot has already been selected");
 				}
-		}
-			//end do...while loop
-			
-
-				for (int a=0; a< gameSpaces.length; a++) {
-					if (noSame(gameSpaces)==true) {
-						System.out.println("You have won the game!");
-					}
-					else {
-
-					}
+				if (turn=='X') {
+					turn='O';
 				}
-				
+				else {
+					turn='X';
+				}	
+			}
 
+		}				
+		//end do...while loop
+
+		display(printBoard(int [][] gameSpaces,int row, int column));
+
+
+		
+		for (int a=0; a< gameSpaces.length; a++) {
+			if (noSame(gameSpaces)==true) {
+				System.out.println("You have won the game!");
+			}
+			else {
+
+			}
 		}
-		//		if (truth==false) {
-		//			System.out.println("This spot has already been selected");
-		//		}
-		//		
-		//		else if (ans==true) {
-		//			System.out.println("You have won the game!");
-		//		}
-		//		else {
-		//			
-		//		}
-		//		
-		//		}
+		
 	
+
+
+	}
+	//		if (truth==false) {
+	//			System.out.println("This spot has already been selected");
+	//		}
+	//		
+	//		else if (ans==true) {
+	//			System.out.println("You have won the game!");
+	//		}
+	//		else {
+	//			
+	//		}
+	//		
+	//		}
+
 
 
 
 	public static boolean gameSpacesX (int row, int column, char[][] gameSpaces) {
 		boolean truth= false;
-		if (gameSpaces[row][column] == 0) {
+		if (gameSpaces[row-1][column-1] == 0) {
 			truth=true;
 			gameSpaces[row-1][column-1] = 'X';
 			if (gameSpaces[row-1][column-1] != 0) {
@@ -93,7 +107,7 @@ public class TicTacToe {
 	public static boolean gameSpacesY (int row, int column) {
 		boolean truth= false;
 		int [][] gameSpaces = new int[3][3];
-		if (gameSpaces[row][column] == 0) {
+		if (gameSpaces[row-1][column-1] == 0) {
 			truth=true;
 
 			if (gameSpaces[row-1][column-1] != 0) {
@@ -105,15 +119,19 @@ public class TicTacToe {
 		}
 		return truth;
 	}
-	
-public static boolean printBoard (int [][] gameSpaces) {
-	for (int z=0; z < 3; z++) { 
-		System.out.println("_" + gameSpaces[0][0] + "_" + gameSpaces[0][1] + "_" + gameSpaces[0][2] );
-		}
-	
-	
-	return false;
-}
+
+	public static void printBoard (int [][] gameSpaces,int row, int column) {
+		
+		System.out.println("_" + gameSpaces[0][0] + "_" + gameSpaces[0][1] + "_" + gameSpaces[0][2]);
+		System.out.println("------------");
+		System.out.println("_" + gameSpaces[1][0] + "_" + gameSpaces[1][1] + "_" + gameSpaces[1][2]);
+		System.out.println("------------");
+		System.out.println("_" + gameSpaces[2][0] + "_" + gameSpaces[2][1] + "_" + gameSpaces[2][2]);
+		
+
+		
+	}
+
 	public static boolean noSame (char [][] gameSpaces) {
 		boolean ans=false;
 		for (int i=0; i < gameSpaces.length; i++) {
@@ -150,8 +168,8 @@ public static boolean printBoard (int [][] gameSpaces) {
 		}
 		return ans;
 	}
-	
-	
+
+
 }
 
 
