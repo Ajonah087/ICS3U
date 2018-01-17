81e153bb1281a087401a2911fb3c20c2a9c0a2eb
343057964 <343057964@C-5LQJHS1.tdsb.on.ca>
2018-01-17 14:55:20 -0500

    doing well enough

--- Unit 3/src/aidan/excercises/CulminatingAssignment_AidanJonah.java
+++ Unit 3/src/aidan/excercises/CulminatingAssignment_AidanJonah.java
@@ -19,8 +19,9 @@ public class CulminatingAssignment_AidanJonah {
 
 	/**
 	 * @param args
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		EV3LargeRegulatedMotor rightMotor= new EV3LargeRegulatedMotor(MotorPort.C);
 		EV3LargeRegulatedMotor leftMotor= new EV3LargeRegulatedMotor(MotorPort.B);
 		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
@@ -28,21 +29,74 @@ public class CulminatingAssignment_AidanJonah {
 		EV3ColorSensor colour = new EV3ColorSensor(SensorPort.S3);
 		System.out.println("Press the square button to start the maze runner program.");
 		Button.waitForAnyPress();
-		
+		rightMotor.forward();
+		leftMotor.forward();
+		maintainDistance(rightMotor,leftMotor,sonar,gyro,colour);
 	}
 	
-	public static void maintainDistance() throws InterruptedException{
-		EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S4);
+	public static void maintainDistance(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, EV3UltrasonicSensor sonar, EV3GyroSensor gyro, EV3ColorSensor colour) throws InterruptedException{
 		float [] distances = new float[1];
 		sonar.fetchSample(distances,0);
 		if (distances[0] < 0.05) {
+			colorCheck(rightMotor, leftMotor, sonar, gyro, colour);
 		}
 	}
 
-	public static void colorCheck() throws InterruptedException{	
-		EV3ColorSensor colour = new EV3ColorSensor(SensorPort.S3);
+	public static void colorCheck(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, EV3UltrasonicSensor sonar, EV3GyroSensor gyro, EV3ColorSensor colour) throws InterruptedException{	
 		float [] sample = new float[1];
 		colour.getColorIDMode().fetchSample(sample,0);
+		
+		if (sample[0]== -1) {
+			rightMotor.forward();
+			leftMotor.forward();
+			Thread.sleep(250);
+		}
+		
+		
+		else if (sample[0]==3) {
+			rightMotor.setSpeed(500);
+			leftMotor.setSpeed(200);
+			
+			rightMotor.forward();
+			leftMotor.forward();
+			Thread.sleep(250);
+		}
+		
+		else if (sample[0]==1) {
+			rightMotor.setSpeed(250);
+			leftMotor.setSpeed(250);
+			
+			rightMotor.backward();
+			leftMotor.backward();
+			Thread.sleep(250);
+			rightMotor.setSpeed(500);
+			leftMotor.setSpeed(200);
+			rightMotor.forward();
+			leftMotor.forward();
+			Thread.sleep(350);
+		}
+		
+		else if(sample[0]==2) {
+			rightMotor.setSpeed(200);
+			leftMotor.setSpeed(500);
+			
+			rightMotor.forward();
+			leftMotor.forward();
+			Thread.sleep(250);
+		}
+		
+		else if(sample[0]==6) {
+			rightMotor.setSpeed(250);
+			leftMotor.setSpeed(250);
+			rightMotor.backward();
+			leftMotor.backward();
+			Thread.sleep(250);
+			rightMotor.setSpeed(200);
+			leftMotor.setSpeed(500);
+			rightMotor.forward();
+			leftMotor.forward();
+			Thread.sleep(350);
+		}
 		
 	}
 	
